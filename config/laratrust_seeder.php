8fb071ca95d596ed43fb07e39cc9989ac55b982c
MahmoudSayed <m.sayedfci@gmail.com>
2019-11-08 00:52:44 +0200

    add admin role

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -4,7 +4,8 @@ return [
     'role_structure' => [
         'super_admin' => [
             'users' => 'c,r,u,d',
-        ]
+        ],
+        'admin' => []
     ],
     'permissions_map' => [
         'c' => 'create',
