cbe3ea24812a3eb01528eb67317d5417a496f9b6
MahmoudSayed <m.sayedfci@gmail.com>
2020-01-20 16:11:44 +0200

    heroku deploy

--- readme.md
+++ readme.md
@@ -1,2 +1,16 @@
 # larapos
-This is laravel admin lte pos
+
+This is an laravel admin dashboard for e-commerce app
+<strong>Follow this steps for setup app:</strong>
+<br>
+
+-   Setup Database
+
+> php artisan migrate:fresh --seed
+
+-   You can login as a Super Admin User by using this data
+
+```
+Email: super_admin@app.com
+Password: asd123
+```
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>AdminLTE 2 | Blank Page</title>
+        <title>{{ config('app.name') }}</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
         {{--<!-- Bootstrap 3.3.7 -->--}}
@@ -89,11 +89,11 @@
     <body class="hold-transition login-page">
         <div class="login-box">
             <div class="login-logo">
-                <a href="javascript:;"><b>Admin</b>LTE</a>
+            <a href="javascript:;"><b>{{ config('app.name') }}</b>
             </div>
             <!-- /.login-logo -->
             <div class="login-box-body">
-                <p class="login-box-msg">Sign in to start your session</p>
+                {{-- <p class="login-box-msg">Sign in to start your session</p> --}}
 
                 <form action="{{ route('login') }}" method="post">
                     @csrf
